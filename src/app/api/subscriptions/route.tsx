d4cc254a02c13f7b1c3ee598aa9a071b825661ee
Simon Oxford <simonoxford99@gmail.com>
2024-11-11 16:38:16 +0000

    route file

--- src/app/api/subscriptions/route.tsx
+++ src/app/api/subscriptions/route.tsx
@@ -1,7 +1,15 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from 'next/server'
 import dbConnect from '@/lib/dbConnect'
 import Subscription from '@/models/Subscription'
 
 export async function POST(request: Request) {
-  await dbConnect()
+  await dbConnect();
+
+  try {
+    const data = await request.json()
+    const newSubscription = await Subscription.create(data)
+    return NextResponse.json({ success: true, data: newSubscription })
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error.message })
+  }
 }
