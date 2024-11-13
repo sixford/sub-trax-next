ac1eb19e3b9720b0b6cc0d5970410ad8db6ee9d5
Simon Oxford <simonoxford99@gmail.com>
2024-11-13 23:03:03 +0000

    Testing API endpoints

--- src/app/api/subscriptions/route.tsx
+++ src/app/api/subscriptions/route.tsx
@@ -2,14 +2,13 @@ import { NextResponse } from 'next/server'
 import dbConnect from '@/lib/dbConnect'
 import Subscription from '@/models/Subscription'
 
-export async function GET(request: Request) {
+export async function GET() {
   await dbConnect()
 
   try {
-    const data = await request.json()
-    const newSubscription = await Subscription.create(data)
-    return NextResponse.json({ success: true, data: newSubscription })
+    const subscriptions = await Subscription.find({})
+    return NextResponse.json({ success: true, data: subscriptions })
   } catch (error) {
-    return NextResponse.json({ success: false, error: error.message })
+    return NextResponse.json({ success: false, error: error.message }, { status: 500 })
   }
 }
