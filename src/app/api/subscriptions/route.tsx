e18bce230e85e061adc1983f1e98db8e73ace1a4
Simon Oxford <simonoxford99@gmail.com>
2024-11-13 23:04:05 +0000

    Added try catch function to create data

--- src/app/api/subscriptions/route.tsx
+++ src/app/api/subscriptions/route.tsx
@@ -12,3 +12,15 @@ export async function GET() {
     return NextResponse.json({ success: false, error: error.message }, { status: 500 })
   }
 }
+
+export async function POST(request: Request) {
+  await dbConnect()
+
+  try {
+    const data = await request.json()
+    const newSubscription = await Subscription.create(data)
+    return NextResponse.json({ success: true, data: newSubscription })
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error.message }, { status: 500 })
+  }
+}
