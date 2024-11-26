dba6996683fa27320b47d8d29863f83287944adc
Simon Oxford <simonoxford99@gmail.com>
2024-11-26 19:38:31 +0000

    Added delete function to router

--- src/app/api/subscriptions/route.tsx
+++ src/app/api/subscriptions/route.tsx
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server'
 import dbConnect from '@/lib/dbConnect'
 import Subscription from '@/models/Subscription'
 
+// Get request function
+
 export async function GET() {
   await dbConnect()
 
@@ -13,6 +15,8 @@ export async function GET() {
   }
 }
 
+// Add Subscription function
+
 export async function POST(request: Request) {
   await dbConnect()
 
@@ -20,6 +24,26 @@ export async function POST(request: Request) {
     const data = await request.json()
     const newSubscription = await Subscription.create(data)
     return NextResponse.json({ success: true, data: newSubscription })
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error.message }, { status: 500 })
+  }
+}
+
+// DELETE function
+
+export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+  await dbConnect()
+
+  const { id } = params
+
+  try {
+    const deletedSubscription = await Subscription.findByIdAndDelete(id);
+
+    if (!deletedSubscription) {
+      return NextResponse.json({ success: false, message: 'Subscription not found' }, { status: 404 })
+    }
+
+    return NextResponse.json({ success: true, message: 'Subscription deleted' })
   } catch (error) {
     return NextResponse.json({ success: false, error: error.message }, { status: 500 })
   }
