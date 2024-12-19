3814e69d9a31eedc8f762cdbb60ffd537069706f
Simon Oxford <simonoxford99@gmail.com>
2024-12-19 13:30:32 +0000

    I + imports and interface for spending chart

---
+++ src/app/components/SpendChart.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+import { Line } from 'react-chartjs-2'
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
+
+interface Subscription {
+  renewalDate: string
+  price: number
+}
